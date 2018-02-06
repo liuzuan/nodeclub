115e16b7eda1b485ebca878e92c6a75514e37b55
liuzuan <467209099@qq.com>
2018-02-07 00:57:40 +0800

    change style

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -15,10 +15,15 @@ const getClientEnvironment = require('./env');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
-const publicPath = paths.servedPath;
+
+const publicPath = './';
+// const publicPath = paths.servedPath;
+
 // Some apps do not use client-side routing with pushState.
 // For these, "homepage" can be set to "." to enable relative asset paths.
+
 const shouldUseRelativeAssetPaths = publicPath === './';
+
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 // const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 const shouldUseSourceMap = false;
