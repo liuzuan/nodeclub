10f729f0d7bbf31f598ba9e2da7209a824ff21a2
liuzuan <liuzuann@gmail.com>
2021-04-11 18:58:06 +0800

    CHG: redux constant

--- src/store/action-type.js
+++ src/store/action-type.js
@@ -1,14 +1,14 @@
 // 保存用户登录信息
-export const SAVEUSERINFO = 'SAVEUSERINFO';
+export const SAVEUSERINFO = 'SAVE_USER_INFO';
 
 // 清除用户登录信息
-export const CLEARUSERINFO = 'CLEARUSERINFO';
+export const CLEARUSERINFO = 'CLEAR_USER_INFO';
 
 //保存首页数据
-export const SAVEHOMESTATE = 'SAVEHOMESTATE';
+export const SAVEHOMESTATE = 'SAVE_HOME_STATE';
 
 // 保存主题详情页数据
-export const SAVETOPICSTATE = 'SAVETOPICSTATE';
+export const SAVETOPICSTATE = 'SAVE_TOPIC_STATE';
 
 
 
